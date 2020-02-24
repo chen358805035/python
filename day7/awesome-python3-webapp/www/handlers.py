15b632ae8c26dcbb94fe746bd5ce581aae5ef2c8
chen358805035 <358805035@qq.com>
2020-02-24 06:27:32 -0800

    编写MVC 编译通过

---
+++ day7/awesome-python3-webapp/www/handlers.py
@@ -0,0 +1,14 @@
+'url handlers'
+
+import re, time, json, logging, hashlib, base64, asyncio
+from coroweb import get, post
+from models import User, Comment, Blog, next_id
+#处理首页URL的函数：
+@get('/')
+async def index(request):
+	users = await User.findAll()
+	return {
+		'__template__' : 'test.html',
+		'users' : users
+	}
+
