96b28785fba9108bb7d23d7a6e4700e7cedd519f
chen358805035 <358805035@qq.com>
2020-02-25 06:41:50 -0800

    构建前端‘

---
+++ day8/awesome-python3-webapp/www/handlers.py
@@ -0,0 +1,24 @@
+'url handlers'
+
+import re, time, json, logging, hashlib, base64, asyncio
+from coroweb import get, post
+from models import User, Comment, Blog, next_id
+#处理首页URL的函数：
+# @get('/')
+# async def index(request):
+# 	users = await User.findAll()
+# 	return {
+# 		'__template__' : 'test.html',
+# 		'users' : users
+# 	}
+
+@get('/')
+async def index(request):
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id = '1', name = 'Test Blog', summary = summary, created_at = time.time()-120),
+		Blog(id = '2', name = 'Something New', summary = summary, created_at = time.time()-3600),
+		Blog(id = '3', name ='Learn Swift', summary = summary, created_at = time.time()-7200)
+	]
+
+	return { '__template__' : 'blogs.html', 'blogs' : blogs}
