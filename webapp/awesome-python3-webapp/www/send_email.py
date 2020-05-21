b603d17d91d07b504e4befe059b0bf94620f1ef9
xiaohu98 <48748940+xiaohu98@users.noreply.github.com>
2020-05-21 13:58:11 +0800

    updata

--- webapp/awesome-python3-webapp/www/send_email.py
+++ webapp/awesome-python3-webapp/www/send_email.py
@@ -4,21 +4,23 @@ from email.mime.text import MIMEText
 from email.utils import parseaddr, formataddr
 import asyncio
 import smtplib
+
+
 def _format_addr(s):
     name, addr = parseaddr(s)
-    return formataddr((Header(name, 'utf-8').encode(),addr))
+    return formataddr((Header(name, 'utf-8').encode(), addr))
+
 
 def send_email(user_name, content, name):
     from_addr = 'c18683346287@163.com'
     password = 'YPNYKMZURQNXXWOZ'
-   
-
+ 
     to_addr = '358805035@qq.com'
 
     smtp_server = 'smtp.163.com'
-    #with open('F:/python/webapp/awesome-python3-webapp/www/templates/blog.html', 'r', encoding='utf-8') as f:
-        
-        #msg = MIMEText(f.read(), 'html', 'utf-8')
+    # with open('F:/python/webapp/awesome-python3-webapp/www/templates/blog.html', 'r', encoding='utf-8') as f:
+
+    #msg = MIMEText(f.read(), 'html', 'utf-8')
     msg = MIMEText(content, 'html', 'utf-8')
     msg['From'] = _format_addr('%s <%s>' % (user_name, from_addr))
     msg['To'] = _format_addr('管理员 <%s>' % to_addr)
